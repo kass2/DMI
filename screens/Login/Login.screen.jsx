11e22fd359df83917ddecf94c7c144e9184ee8fe
Sergio <sergiopsc2@gmail.com>
2021-12-06 23:53:57 -0600

    Both projects

--- screens/Login/Login.screen.jsx
+++ screens/Login/Login.screen.jsx
@@ -30,7 +30,7 @@ const LoginPage = () => {
 
   useEffect(() => {
     if(state){
-      
+      console.log(state.user)
       navigation.navigate('Home',{itemId: state.user.uid, photoURL: state.user.photoURL, email: state.user.email, navi: navigation});
     }
   }, [state]); 
